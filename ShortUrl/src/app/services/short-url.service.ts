9cb0433c08dd442ac232ffd23b6d0f03cd74a489
LucianoBertero <luchobertero@gmail.com>
2024-02-09 02:03:09 -0300

    cambios de ruta

--- ShortUrl/src/app/services/short-url.service.ts
+++ ShortUrl/src/app/services/short-url.service.ts
@@ -9,20 +9,18 @@ export class ShortUrlService {
 
   acortar(url: string) {
     console.log('entro');
-    this.http
-      .post('https://short-url-backend.fly.dev/', { url: url })
-      .subscribe(
-        (response) => {
-          console.log('respondio');
-          console.log('Respuesta del backend:', response);
-          // Puedes mostrar un mensaje de éxito al usuario o realizar otras acciones según la respuesta
-        },
-        (error) => {
-          // Manejar cualquier error de la solicitud
-          console.error('Error al enviar la solicitud:', error);
-          console.log('Norespondio');
-          // Puedes mostrar un mensaje de error al usuario o realizar otras acciones
-        }
-      );
+    this.http.post('https://backendkkk.fly.dev/', { url: url }).subscribe(
+      (response) => {
+        console.log('respondio');
+        console.log('Respuesta del backend:', response);
+        // Puedes mostrar un mensaje de éxito al usuario o realizar otras acciones según la respuesta
+      },
+      (error) => {
+        // Manejar cualquier error de la solicitud
+        console.error('Error al enviar la solicitud:', error);
+        console.log('Norespondio');
+        // Puedes mostrar un mensaje de error al usuario o realizar otras acciones
+      }
+    );
   }
 }
